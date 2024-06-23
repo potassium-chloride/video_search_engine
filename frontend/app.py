66a05be0de3e12a669cd2a3061db379135fd8002
potassium-chloride <34630909+potassium-chloride@users.noreply.github.com>
2024-06-24 01:22:35 +0300

    Update app.py
    Add some comments

--- frontend/app.py
+++ frontend/app.py
@@ -2,10 +2,11 @@
 import sys,time,os,tempfile,requests,json
 from flask import Flask
 from flask import request
-
 import numpy as np
-
 import pickle
+
+# Подгрузка ранее проиндексированных видео
+# Здесь это дамп списка словарей, но решение дружелюбно к векторным базам данных и обычному поиску по тексту
 
 dict_store = []
 try:
@@ -16,9 +17,13 @@ except:
 
 all_urls = [o['url'] for o in dict_store]
 
+# На моём CPU текстовый энкодер текста исполняется за 300 мс, кеширование позволяет сэкономить время
 text2vec_cache = {}
 
 def text2vec(text):
+	'''
+	Текстовый энкодер CLIP, обращение к API бекенда-сервера
+	'''
 	global text2vec_cache
 	if text in text2vec_cache:
 		return text2vec_cache[text]
@@ -31,6 +36,9 @@ def text2vec(text):
 doc2vec_cache = {}
 
 def doc2vec(text):
+	'''
+	Текстовый энкодер doc2vec, обращение к API бекенда-сервера
+	'''
 	global doc2vec_cache
 	if text in doc2vec_cache:
 		return doc2vec_cache[text]
@@ -41,6 +49,9 @@ def doc2vec(text):
 	return res
 
 def video2dict(videopath,description="",stride=5,K=5):
+	'''
+	Обработка видео, обращение к API бекенда-сервера
+	'''
 	res = requests.get("http://127.0.0.1:6000/encodeVideoByLink",params=[
 		("url",videopath),
 		("description",description),
@@ -60,18 +71,30 @@ def video2dict(videopath,description="",stride=5,K=5):
 import torch
 import torch.nn as nn
 
+# Ранжирование видео в выдаче зависит от запроса.
+# Здесь предлагается простая модель выбора коэффициентов, с которыми брать каждый из параметров близости
+# Предобученные нами модели лежат в папке models
+# Pipeline: запрос -> вектор -> понижение размерности с помощью PCA -> наша модель -> коэффициенты, с которыми свернуть вектора выдачи -> отсортировать выдачу
+# Замечане 1, что при масштабировании решения эту модель было бы здорово усложнить,
+# а запросы к БД (FAISS и пр.) можно делать асинхронно, не дожидаясь результатов от этой модели
+
 sorter_model_doc = nn.Sequential(
 	nn.Linear(5,11),
 	nn.Dropout(p=0.2),
-	nn.Sigmoid() # -> (0,1) important!
+	nn.Sigmoid() # -> (0,1) Важно! Модель не допускает отрицательных или запредельно больших коэффициентов, это сделает обучение невозможным
 )
 
-print("sorter_model:",sorter_model_doc.load_state_dict(torch.load('models/sorter_model_doc.pt')) )
+print("sorter_model:", sorter_model_doc.load_state_dict(torch.load('models/sorter_model_doc.pt')) )
 
 pca_M_doc = np.loadtxt("models/pca_M_doc.dat")
 pca_mu_doc = np.loadtxt("models/pca_mu_doc.dat")
 
 def getCoefsByPrompt_doc(query):
+	'''
+	Преобразует текстовый запрос в коэффициенты от модели
+	Вход: текст или вектор текстового эмбеддинга
+	Выход: NumPy массив из 11 чисел
+	'''
 	if type(query)==str:
 		query = doc2vec(query)['embedding']
 	sorter_model_doc.eval()
@@ -81,11 +104,12 @@ def getCoefsByPrompt_doc(query):
 		coefs = sorter_model_doc(enc)
 		return coefs.numpy()[0]
 
+# Аналогично для текстового энкодера CLIPа похожая модель:
 pca_M_clip = np.loadtxt("models/pca_M_clip.dat")
 pca_mu_clip = np.loadtxt("models/pca_mu_clip.dat")
 
 sorter_model_clip = nn.Sequential(
-	nn.Linear(5,11),
+	nn.Linear(6,11),
 	nn.Dropout(p=0.2),
 	nn.Sigmoid() # -> (0,1) important!
 )
@@ -113,10 +137,10 @@ def addVideo():
 	start_t = time.time()
 	url = request.args.get('url')
 	description = request.args.get('description',"")
-	stride = int(request.args.get('stride',5))
-	K = int(request.args.get('K',5))
-	force = bool(request.args.get('force',False))
-	if url == "":
+	stride = int(request.args.get('stride',5)) # Опционально
+	K = int(request.args.get('K',5)) # Опционально
+	force = bool(request.args.get('force',False)) # Принудительно поторно индексирует видео, если оно уже есть в индексе
+	if url == "": # Если в поле URL пусто, пытаемся извлечь данные из поля данных POST-запроса
 		post_data = request.get_json()
 		url = post_data['url']
 		description = post_data['description']
@@ -131,7 +155,7 @@ def addVideo():
 			raise Exception(tmp_obj['error'])
 		tmp_obj['url'] = url
 		tmp_obj['desc_orig'] = description
-		dict_store.append(tmp_obj)
+		dict_store.append(tmp_obj) # После отработки этого запроса видео уже можно искать в поиске
 		all_urls.append(url)
 		res['result'] = 'ok'
 		res['storelen'] = len(dict_store)
@@ -144,8 +168,17 @@ def addVideo():
 	return res
 
 def preprocessVideoForQuery(iVideo,clip_emb,doc_emb,doc_lemmed,doc_words):
-	# 1) Preprocess query
-	# 2) Feature extracting
+	'''
+	Извлечение степени схожести видео на текстовый запрос.
+	При масштабировании перенести эту функцию на движок БД
+	Входы:
+	iVideo -- словарь со свойствами видео
+	clip_emb -- текстовый эмбеддинг от CLIP
+	doc_emb -- текстовый эмбеддинг от doc2vec
+	doc_lemmed -- множество лемматизированных слов
+	doc_words -- слова запроса без лемматизации
+	Выход: список из 11 чисел не более 1 каждое
+	'''
 	features = []
 	clip_innerprod = iVideo['clip_embeddings'].dot(clip_emb)
 	features.append(clip_innerprod.min())
@@ -173,7 +206,10 @@ def preprocessVideoForQuery(iVideo,clip_emb,doc_emb,doc_lemmed,doc_words):
 import re
 url_parse = re.compile("https\\:\\/\\/\\S+fhd\.mp4")
 
-def combo_search(query,k=10): # sum
+def combo_search(query,k=10):
+	'''
+	Поиск топ k видео по текстовому запросу query
+	'''
 	start_t = time.time()
 	clip_emb = text2vec(query.replace("#",""))
 	doc_enc = doc2vec(query)
@@ -181,7 +217,7 @@ def combo_search(query,k=10): # sum
 	doc_lemmed = set(doc_enc['lemmed'] if 'lemmed' in doc_enc else [])
 	doc_words = set(query.replace(",","").lower().split(" "))
 	urls = url_parse.findall(query)
-	if len(urls)>0:
+	if len(urls)>0: # Фича поиска похожих видео, протестировано слабо, но результатом я доволен
 		urls_len = sum([len(u) for u in urls])
 		clip_emb *= (len(query)-urls_len)/len(query)
 		doc_emb *= (len(query)-urls_len)/len(query)
@@ -192,7 +228,7 @@ def combo_search(query,k=10): # sum
 	preprocessed_videos_codes = []
 	distances_list = []
 	hashtags = [w for w in doc_words if w.startswith('#')]
-	weights = 0.6*getCoefsByPrompt_doc(doc_emb)
+	weights = 0.6*getCoefsByPrompt_doc(doc_emb) # Получение коэффициентов для ранжирования
 	weights += 0.4*getCoefsByPrompt_clip(clip_emb)
 	encoding_time = time.time()-start_t
 	search_time = time.time()
@@ -211,7 +247,7 @@ def combo_search(query,k=10): # sum
 	weighted_sum = preprocessed_videos_codes.dot(weights)
 	for i in range(len(distances_list)):
 		distances_list[i]['weighted'] = weighted_sum[i]
-	distances_list.sort(key = lambda x:(-x['hashtags'],-x['weighted']) )
+	distances_list.sort(key = lambda x:(-x['hashtags'],-x['weighted']) ) # Сначала упорядочить по хештегам дословно
 	search_time = time.time()-search_time
 	return distances_list[:k],weights.tolist(),encoding_time,search_time
 
@@ -223,7 +259,7 @@ def searchVideoHandle():
 	try:
 		learn_data = request.get_data(as_text=True)
 		print("data:",learn_data)
-		if learn_data is not None and len(learn_data)>5:
+		if learn_data is not None and len(learn_data)>5: # Записать голоса релевантноси в файл для дообучения моделей
 			learn_data = learn_data.split(",")
 			with open("search_alignment.csv","a") as fl:
 				for l in learn_data:
